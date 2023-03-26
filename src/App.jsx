9ac75eb53a7f6e72d060af497cdaab2ac6067ea2
warronyiang15 <warronyiang15@gmail.com>
2023-03-26 23:26:28 +0800

    First Upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>HI, I am Warron Yiang Wai Loon (b09902078)</h1>
+      <h2>Currently Study at National Taiwan University</h2>
+      <h3>Department: Computer Science and Information Engineering</h3>
+    </div>
+  )
+}
+
+export default App
